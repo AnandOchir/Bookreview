c8b69d3b3f21a7ab1a431b346a3e1e21dc9468ab
radnaasuren <radnaaerjaal93@gmail.com>
2022-01-18 22:31:19 -0800

    unready bookDetai

--- bookreview-frontend/src/App.js
+++ bookreview-frontend/src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import { HomePage, BookDetail, SignUpPage, LoginPage } from './pages';
 import './styles/main.scss'
+import "@fontsource/roboto";
 
 const App = () => {
   return (
